95c31d4c65e912399bd7bac3a6fb8cf4601a7669
juan <juan.pedro.casanas99@gmail.com>
2021-11-06 15:35:04 -0300

    minor changes, still WIP

--- scripts.js
+++ scripts.js
@@ -1,77 +1,85 @@
-(
+/*/(
   function () {
     const gameOptions = (function () {
+/*/
+//cacheDOM//
+const modeSelect = document.getElementById('modeSelect');
+const difficultySelect = document.getElementById('difficultySelect');
+const playButton = document.getElementById('playButton');
+const resetButton = document.getElementById('resetButton');
+const boardSquares = document.getElementsByClassName('boardSquare');
 
-      //cacheDOM//
-      const modeSelect = document.getElementById('modeSelect');
-      const difficultySelect = document.getElementById('difficultySelect');
-      const playButton = document.getElementById('playButton');
-      const resetButton = document.getElementById('resetButton');
-      const boardSquares = document.getElementsByClassName('boardSquare');
-
-      //TURN FLAGS => MAYBE IT DOESN'T GO HERE//
-      let pOneTurn;
-      let pTwoTurn;
-
-      //Event listeners//
-      playButton.addEventListener('click', runGame);
-      resetButton.addEventListener('click', resetGame);
-
-      //Functions//
-      function handleTurns() {
-        switch (pOneTurn) {
-          case undefined:
-            pOneTurn = true;
-            pTwoTurn = false;
-            return { pOneTurn, pTwoTurn, };
-
-          case true:
-            pOneTurn = false;
-            pTwoTurn = true;
-            return { pOneTurn, pTwoTurn, };
-
-          case false:
-            pOneTurn = true;
-            pTwoTurn = false;
-            return { pOneTurn, pTwoTurn, };
-
-          default:
-            alert('Something went wrong while handling turns');
-            resetGame();
-            break;
-        }
-      }
-
-      function makeGridClickable(turns) {
-        let boardSquaresArray = Array.from(boardSquares);
-        boardSquaresArray.forEach((e) => {
-          e.addEventListener('click', placeFigure.bind(this, e, turns));
-        })
-      }
-
-      function placeFigure(e, turns) {
-        if (turns.pOneTurn === true) {
-          e.textContent = 'X'
-          handleTurns();
-        } else if (turns.pTwoTurn === true) {
-          e.textContent = 'O'
-          handlesTurns();
-        }
-      }
+//TURN FLAGS => MAYBE IT DOESN'T GO HERE//
 
 
+//Event listeners//
+playButton.addEventListener('click', runGame);
+resetButton.addEventListener('click', resetGame);
 
-      function runGame() { }
-      function resetGame() { }
+//Functions//
+function handleTurns(pOneTurn, pTwoTurn) {
+  switch (pOneTurn) {
+    case undefined:
+      pOneTurn = true;
+      pTwoTurn = false;
+      return { pOneTurn, pTwoTurn, };
+
+    case true:
+      pOneTurn = false;
+      pTwoTurn = true;
+      return { pOneTurn, pTwoTurn, };
+
+    case false:
+      pOneTurn = true;
+      pTwoTurn = false;
+      return { pOneTurn, pTwoTurn, };
+
+    default:
+      alert('Something went wrong while handling turns');
+      resetGame();
+      break;
+  }
+}
+
+function makeGridClickable(turns) {
+  let boardSquaresArray = Array.from(boardSquares);
+  boardSquaresArray.forEach((element) => {
+    let boundedFunction = placeFigure.bind(this, element, turns);
+    element.removeEventListener('click',boundedFunction);
+    element.addEventListener('click', boundedFunction);
+  })
+}
+
+function placeFigure(element, turns) {
+  if (turns.pOneTurn === true) {
+    element.textContent = 'X';
+    turns = handleTurns(turns.pOneTurn, turns.pTwoTurn);
+    makeGridClickable(turns)
+  } else if (turns.pTwoTurn === true) {
+    element.textContent = 'O'
+    turns = handleTurns(turns.pOneTurn, turns.pTwoTurn);
+    makeGridClickable(turns);
+  }
+}
 
 
 
+function runGame() {
+  let pOneTurn;
+  let pTwoTurn;
+  let turns = handleTurns(pOneTurn, pTwoTurn);
+  makeGridClickable(turns);
+}
+function resetGame() { }
 
 
 
 
 
+
+
+/*/
     }
     )()
   }
-)()
+)()/*/
