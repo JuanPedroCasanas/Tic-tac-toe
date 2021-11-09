49f9d9516f481e17734a6c51d1824f2f459c932c
juan <juan.pedro.casanas99@gmail.com>
2021-11-09 15:31:11 -0300

    Major code refactors, looking clean now, win conditions can probably be optimized somehow Need to work on the reset game func

--- scripts.js
+++ scripts.js
@@ -7,9 +7,10 @@ const modeSelect = document.getElementById('modeSelect');
 const difficultySelect = document.getElementById('difficultySelect');
 const playButton = document.getElementById('playButton');
 const resetButton = document.getElementById('resetButton');
-const boardSquares = document.getElementsByClassName('boardSquare');
+const boardSquares = Array.from(document.getElementsByClassName('boardSquare'));
 
 //TURN FLAGS => MAYBE IT DOESN'T GO HERE//
+let turn = 0;
 
 
 //Event listeners//
@@ -17,64 +18,71 @@ playButton.addEventListener('click', runGame);
 resetButton.addEventListener('click', resetGame);
 
 //Functions//
-function handleTurns(pOneTurn, pTwoTurn) {
-  switch (pOneTurn) {
-    case undefined:
-      pOneTurn = true;
-      pTwoTurn = false;
-      return { pOneTurn, pTwoTurn, };
-
-    case true:
-      pOneTurn = false;
-      pTwoTurn = true;
-      return { pOneTurn, pTwoTurn, };
-
-    case false:
-      pOneTurn = true;
-      pTwoTurn = false;
-      return { pOneTurn, pTwoTurn, };
-
-    default:
-      alert('Something went wrong while handling turns');
-      resetGame();
-      break;
+function playerFactory(avatar, isActive) {
+  let newPlayerState = {
+    avatar,
+    isActive,
   }
+  return {
+    get avatar() { return newPlayerState.avatar },
+    get isActive() { return newPlayerState.isActive },
+    toggleActiveness: () => { newPlayerState.isActive = !newPlayerState.isActive; },
+  };
 }
 
-function makeGridClickable(turns) {
-  let boardSquaresArray = Array.from(boardSquares);
-  boardSquaresArray.forEach((element) => {
-    let boundedFunction = placeFigure.bind(this, element, turns);
-    element.removeEventListener('click',boundedFunction);
-    element.addEventListener('click', boundedFunction);
+function makeGridClickable(players) {
+  boardSquares.forEach((e) => {
+    e.addEventListener('click', () => { placeFigure(players, e) });
   })
 }
 
-function placeFigure(element, turns) {
-  if (turns.pOneTurn === true) {
-    element.textContent = 'X';
-    turns = handleTurns(turns.pOneTurn, turns.pTwoTurn);
-    makeGridClickable(turns)
-  } else if (turns.pTwoTurn === true) {
-    element.textContent = 'O'
-    turns = handleTurns(turns.pOneTurn, turns.pTwoTurn);
-    makeGridClickable(turns);
+function placeFigure(players, e) {
+  if (e.textContent != '') {
+  } else if (players[0].isActive) {
+    e.textContent = 'X';
+    turn++
+    checkWinConditions(players[0]);
+    checkWinConditions(players[1]);
+    players.forEach((e) => { e.toggleActiveness() });
+
+  } else if (players[1].isActive) {
+    e.textContent = 'O'
+    turn++
+    checkWinConditions(players[0]);
+    checkWinConditions(players[1]);
+    players.forEach((e) => { e.toggleActiveness() });
   }
 }
 
-
+function checkWinConditions(player) {
+  let x = []
+  boardSquares.forEach((e) => x.push(e.textContent));
+  if (x[0] + x[1] + x[2] === player.avatar + player.avatar + player.avatar ||
+    x[3] + x[4] + x[5] === player.avatar + player.avatar + player.avatar ||
+    x[6] + x[7] + x[8] === player.avatar + player.avatar + player.avatar ||
+    x[0] + x[3] + x[6] === player.avatar + player.avatar + player.avatar ||
+    x[1] + x[4] + x[7] === player.avatar + player.avatar + player.avatar ||
+    x[2] + x[5] + x[8] === player.avatar + player.avatar + player.avatar ||
+    x[0] + x[4] + x[8] === player.avatar + player.avatar + player.avatar ||
+    x[6] + x[4] + x[2] === player.avatar + player.avatar + player.avatar) {
+    alert(`Player ${player.avatar} wins`);
+    resetGame()
+  } else if (turn === 9) {
+    console.log('draw');
+    resetGame()
+  } else {
+  }
+}
 
 function runGame() {
-  let pOneTurn;
-  let pTwoTurn;
-  let turns = handleTurns(pOneTurn, pTwoTurn);
-  makeGridClickable(turns);
+  let players = [playerFactory('X', true), playerFactory('O', false)]
+  makeGridClickable(players);
 }
-function resetGame() { }
 
-
-
-
+function resetGame() {
+  turn = 0;
+  boardSquares.forEach((e)=>e.textContent = '');
+ }
 
 
 
@@ -83,3 +91,6 @@ function resetGame() { }
     )()
   }
 )()/*/
+
+
+
